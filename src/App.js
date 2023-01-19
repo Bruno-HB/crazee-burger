462dcbb45d96ad7f987c7910b5d3aee66c543a84
Bruno-HB <contact.brunohb@gmail.com>
2023-01-19 12:36:33 +0100

    ✨ LOGIN > Create LoginPage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import { useState } from "react";
+import LoginPage from "./components/pages/LoginPage";
 
 function App() {
   // state
-    const [name, setName] = useState("")
-    const [text, setText] = useState("")
-  // comportements
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      setName(text)
-      alert(`Bonjour ${text}`)
-      setText("")
-    }
 
-    const handleChange = (e) => {
-      setText(e.target.value)
-    }
+  // comportements
+
   // affichage 
 
-  return (
-    <>
-      <h1>Bienvenue chez nous !</h1>
-      <h2>Connectez-vous</h2>
-      <form onSubmit={ handleSubmit }>
-      <input type="text" placeholder="Entrez votre prénom..." required value={ text } onChange={ handleChange }></input>
-      <button>Accédez à votre espace</button>
-      </form>
-    </>
-  )
+  return <LoginPage />
 }
 export default App;
