31c1e56666e67b53cd35e0662c60ccc07cccf510
Bruno-HB <contact.brunohb@gmail.com>
2023-01-21 14:26:17 +0100

    ✨ F02: Add ErrorPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import LoginPage from "./components/pages/login/LoginPage";
 import {Routes, Route} from "react-router-dom";
 import OrderPage from "./components/pages/order/OrderPage";
-
+import ErrorPage from "./components/pages/error/ErrorPage";
 
 
 
@@ -16,6 +16,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LoginPage />}/>
       <Route path="/order/:username" element={<OrderPage />} />
+      <Route path="/*" element={<ErrorPage/>} />
     </Routes>
   )
 
