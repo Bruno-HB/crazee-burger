5c1525bca3ccd9082619296151217e4b5699f92a
Bruno-HB <contact.brunohb@gmail.com>
2023-01-22 16:57:20 +0100

    💄 F03: APP > Add background image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import LoginPage from "./components/pages/login/LoginPage";
 import {Routes, Route} from "react-router-dom";
 import OrderPage from "./components/pages/order/OrderPage";
 import ErrorPage from "./components/pages/error/ErrorPage";
+import styled from "styled-components";
 
 
 
@@ -13,12 +14,20 @@ function App() {
   // affichage 
 
   return (
+    <AppStyled>
     <Routes>
       <Route path="/" element={<LoginPage />}/>
       <Route path="/order/:username" element={<OrderPage />} />
       <Route path="/*" element={<ErrorPage/>} />
     </Routes>
+    </AppStyled>
   )
 
 }
 export default App;
+
+const AppStyled = styled.div`
+  background-image: url("/F03 burger-background.jpg");
+  background-size: cover;
+  height: 100vh;
+`;
