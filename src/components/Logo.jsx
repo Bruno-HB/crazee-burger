5ac582b393b5d6e9edfc611b3fe14b36ba384dd3
Bruno-HB <contact.brunohb@gmail.com>
2023-01-22 18:14:15 +0100

    ✨ F03: LOGO > Change font-weight & add alt to img

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,13 +4,13 @@ import { theme } from '../theme';
 
 export default function Logo() {
   return (
-    <LogoStyled>CRAZEE <img src='/F03 logo-orange.png' /> BURGER</LogoStyled>
+    <LogoStyled>CRAZEE <img src='/F03 logo-orange.png' alt="Logo of Crazee Burger" /> BURGER</LogoStyled>
   )
 }
 
 const LogoStyled = styled.div`
 font-family: 'Amatic SC';
-font-weight: 700;
+font-weight: ${theme.weights.bold};
 color: ${theme.colors.primary};
 font-size: ${theme.fonts.P6};
 display: flex;
--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../../theme";
+import Logo from "../../Logo";
+import { BsPersonCircle } from "react-icons/bs";
+
 
 export default function LoginForm() {
     // state
@@ -19,12 +24,94 @@ export default function LoginForm() {
 
     // affichage
     return (
-        <form onSubmit={ handleSubmit }>
+        <>
+        <LoginFormStyled onSubmit={ handleSubmit }>
+            <Logo />
             <h1>Bienvenue chez nous !</h1>
             <br/>
+            <div className="line"></div>
             <h2>Connectez-vous</h2>
-            <input type="text" placeholder="Entrez votre prénom..." required value={ username } onChange={ handleChange }></input>
-            <button>Accédez à votre espace</button>
-        </form>
+            <div className="input">
+                <BsPersonCircle className="icon"/>
+                <input type="text" placeholder="Entrez votre prénom" required value={ username } onChange={ handleChange }></input>
+            </div>
+            <div className="buttonStyle">
+                <button>Accéder à votre espace {">"}</button>
+            </div>
+        </LoginFormStyled>
+        </>
     )
 }
+
+const LoginFormStyled = styled.form`
+font-family: 'Amatic SC';
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+color: ${theme.colors.white};
+text-transform: uppercase;
+h1{
+    margin-top: 0;
+    font-size: ${theme.fonts.P5};
+  }
+  .line{
+    width: 35%;
+    border: 2px solid ${theme.colors.primary};
+}
+h2{
+    font-size: ${theme.fonts.P4};
+}
+
+.input, .buttonStyle {
+    font-family: 'Open Sans', sans-serif;
+    height: ${theme.spacing.xl};
+    border-radius: ${theme.borderRadius.round};
+    background-color: ${theme.colors.background_white};
+    margin-bottom: ${theme.spacing.sm};
+    font-size: ${theme.fonts.P3};
+}
+
+.input{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 35%;
+}
+.icon {
+    color: ${theme.colors.greyDark};
+    margin-right: ${theme.spacing.sm};
+}
+input { 
+    border: none;
+    background-color: ${theme.colors.background_white};
+    border-radius: ${theme.borderRadius.round};
+    height: 35%;
+    width: 70%;
+}
+
+input:focus {
+    outline: none;
+    border: 2px solid ${theme.colors.blue};
+}
+
+.buttonStyle {
+    border: 1px solid ${theme.colors.primary};
+    width: 35%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+button {
+    border-radius: ${theme.borderRadius.round};
+    cursor: pointer;
+    border: none;
+    height: 100%;
+    width: 100%;
+    font-size: ${theme.fonts.P1};
+    font-weight: ${theme.weights.bold};
+    color: ${theme.colors.primary_burger};
+    background-color: ${theme.colors.background_white};
+}
+`;
