788ed84e785bde29cb51ce9abc33a6158ef80292
Bruno-HB <contact.brunohb@gmail.com>
2023-01-23 17:36:48 +0100

    💄 F03: DESIGN SYSTEM > Add line separator color

--- src/theme/index.js
+++ src/theme/index.js
@@ -6,6 +6,7 @@ const colors = {
   background_dark: "#292729",
   primary: "#ffa01b",
   primary_burger: "#ff9f1b",
+  line_separator: "#f56a2c",
   green: "green",
   success: "#60bd4f",
   red: "#e25549",
