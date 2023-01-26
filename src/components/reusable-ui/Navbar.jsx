0538e5a156affaf433bae9b696e613be217e2a3b
Bruno-HB <contact.brunohb@gmail.com>
2023-01-26 15:32:13 +0100

    ♻️ F04 : ORDERPAGE > Navbar > Create new component for userbox

--- src/components/reusable-ui/Navbar.jsx
+++ src/components/reusable-ui/Navbar.jsx
@@ -1,20 +1,14 @@
-import { Link } from "react-router-dom";
-import { BsPersonCircle } from "react-icons/bs"
 import styled from "styled-components";
 import Logo from './Logo'
 import { theme } from "../../theme";
+import { BsPersonCircle } from "react-icons/bs"
+import UserConnected from "./UserConnected";
 
 export default function Navbar({username }) {
   return (
     <NavbarStyled>
         <Logo className="logo"/>
-        <div className="user-box">
-            <div className="user-disconnect">
-                <span>Hey, <span className="username">{username}</span> </span>
-                <Link to='/' className="link-to-disconnect">Se déconnecter</Link>
-            </div>
-            <BsPersonCircle className="icon" />
-        </div>
+        <UserConnected username={username} Icon={<BsPersonCircle className="icon" />}/>
     </NavbarStyled>
   )
 }
@@ -27,34 +21,7 @@ const NavbarStyled = styled.div`
     background: white;
     border-radius: 15px 15px 0px 0px;
     justify-content: space-between;
-    .user-box {
-        display: flex;
-        color: #747B91;
-        /* flex-direction: column; */
-        align-items: center;
-        .icon {
-            height: 36px;
-            width: 36px;
-            /* color: #747B91; */
-            padding-right: 70px;
-        }
-        .username {
-            color: ${theme.colors.primary};
-            font-weight: ${theme.fonts.weights.bold};
-        }
-        .user-disconnect {
-            padding-right: 10px;
-            display: flex;
-            flex-direction: column;
-            align-items: flex-end;
-        }
-    }
     .logo {
-        padding-left: 20px;
-    }
-    .link-to-disconnect {
-        text-decoration: none;
-        color: #747B91;
-        font-size: ${theme.fonts.size.XXS};
+        padding-left: ${theme.spacing.md};
     }
 `;
