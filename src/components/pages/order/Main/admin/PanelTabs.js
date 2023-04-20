1b1d9461aef37377b41eadea8aa7b546b4d5a31e
Bruno-HB <contact.brunohb@gmail.com>
2023-04-21 00:24:45 +0200

    ♻️ F07: ADMIN  > Add scalable implementation

--- src/components/pages/order/Main/admin/PanelTabs.js
+++ src/components/pages/order/Main/admin/PanelTabs.js
@@ -4,63 +4,39 @@ import Tab from "../../../../reusable-ui/Tab";
 import { FiChevronDown } from "react-icons/fi";
 import { FiChevronUp } from "react-icons/fi";
 import { theme } from "../../../../../theme";
-import { AiOutlinePlus } from "react-icons/ai";
-import { MdModeEditOutline } from "react-icons/md";
 import OrderContext from "../../../../../context/OrderContext";
+import { getTabsConfig } from "./getTabsConfig";
 
 export default function PanelButtons() {
   //state
   const {
     isCollapsed,
     setIsCollapsed,
-    isEditSelected,
-    setIsEditSelected,
-    isAddSelected,
-    setIsAddSelected,
+    currentTabSelected,
+    setCurrentTabSelected,
   } = useContext(OrderContext);
 
   //comportement
   const selectTab = (tabSelected) => {
     setIsCollapsed(false);
-    if (tabSelected === "add") {
-      setIsAddSelected(true);
-      setIsEditSelected(false);
-    }
-
-    if (tabSelected === "edit") {
-      setIsAddSelected(false);
-      setIsEditSelected(true);
-    }
+    setCurrentTabSelected(tabSelected);
   };
-  const tabsConfig = [
-    {
-      label: "",
-      Icon: !isCollapsed ? <FiChevronDown /> : <FiChevronUp />,
-      onClick: () => setIsCollapsed(!isCollapsed),
-      className: isCollapsed ? "is-active" : "",
-    },
-    {
-      label: "Ajouter un produit",
-      Icon: <AiOutlinePlus />,
-      onClick: () => selectTab("add"),
-      className: isAddSelected ? "is-active" : "",
-    },
-    {
-      label: "Modifier un produit",
-      Icon: <MdModeEditOutline />,
-      onClick: () => selectTab("edit"),
-      className: isEditSelected ? "is-active" : "",
-    },
-  ];
+  const tabs = getTabsConfig(currentTabSelected);
   return (
     <PanelButtonsStyled>
-      {tabsConfig.map((tab) => {
+      <Tab
+        label={""}
+        Icon={!isCollapsed ? <FiChevronDown /> : <FiChevronUp />}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className={isCollapsed ? "is-active" : ""}
+      />
+      {tabs.map((tab) => {
         return (
           <Tab
             label={tab.label}
             Icon={tab.Icon}
-            onClick={tab.onClick}
-            className={tab.className}
+            onClick={() => selectTab(tab.index)}
+            className={currentTabSelected === tab.index ? "is-active" : ""}
           />
         );
       })}
---
+++ src/components/pages/order/Main/admin/getTabsConfig.js
@@ -0,0 +1,21 @@
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
+
+export const getTabsConfig = (currentTabSelected) => [
+  //   {
+  //     label: "",
+  //     Icon: !isCollapsed ? <FiChevronDown /> : <FiChevronUp />,
+  //     onClick: () => setIsCollapsed(!isCollapsed),
+  //     className: isCollapsed ? "is-active" : "",
+  //   },
+  {
+    index: "add",
+    label: "Ajouter un produit",
+    Icon: <AiOutlinePlus />,
+  },
+  {
+    index: "edit",
+    label: "Modifier un produit",
+    Icon: <MdModeEditOutline />,
+  },
+];
