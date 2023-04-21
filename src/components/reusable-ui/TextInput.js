a511fc9aba1be2c3dcdcb5812beb958c96bf15fd
Bruno-HB <contact.brunohb@gmail.com>
2023-04-21 08:44:04 +0200

    🐛 F07: LOGINFORM > init state of value with a name

--- src/components/reusable-ui/TextInput.js
+++ src/components/reusable-ui/TextInput.js
@@ -5,7 +5,7 @@ export default function TextInput({ value, onChange, Icon, ...extraProps }) {
   return (
     <TextInputStyled>
       {Icon && Icon}
-      <input onChange={onChange} type="text" {...extraProps} />
+      <input value={value} onChange={onChange} type="text" {...extraProps} />
     </TextInputStyled>
   );
 }
