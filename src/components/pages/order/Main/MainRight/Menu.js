d5fad4da18c754e5d2be9ab34c6deee40cb3709e
Bruno-HB <contact.brunohb@gmail.com>
2023-05-08 13:04:06 +0200

    ✨ F08: MENU > Add Menu Empty message different if admin is on or off.

--- src/components/pages/order/Main/MainRight/Menu.js
+++ src/components/pages/order/Main/MainRight/Menu.js
@@ -10,6 +10,7 @@ import {
 import MenuContext from "../../../../../context/MenuContext";
 import OrderContext from "../../../../../context/OrderContext";
 import PrimaryButton from "../../../../reusable-ui/PrimaryButton";
+import { fakeMenu } from "../../../../../fakeData/fakeMenu";
 
 export default function Menu() {
   const { menu, setMenu } = useContext(MenuContext);
@@ -37,7 +38,28 @@ export default function Menu() {
             />
           );
         })}
-      {menu.length === 0 && `${isModeAdmin ? `test` : "sismique"}`}
+      {menu.length === 0 && (
+        <div className="empty-menu">
+          {isModeAdmin && (
+            <div>
+              <h1>Le menu est vide ?</h1>
+              <h2>Cliquez ci-dessous pour le réinitialiser</h2>
+              <PrimaryButton
+                className={"generate-button"}
+                label="Générer de nouveaux  produits"
+                onClick={() => setMenu(fakeMenu.MEDIUM)}
+              />
+            </div>
+          )}
+          {!isModeAdmin && (
+            <div>
+              <h1>Victime de notre succès ! :D</h1>
+              <h2>de nouvelles recettes sont en cours de préparation.</h2>
+              <h2>à très vite !</h2>
+            </div>
+          )}
+        </div>
+      )}
     </MenuStyled>
   );
 }
@@ -51,4 +73,32 @@ const MenuStyled = styled.div`
   justify-items: center;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   overflow-y: scroll;
+
+  .empty-menu {
+    text-align: center;
+    min-width: 400px;
+    margin: auto;
+    padding: 40px ${theme.spacing.lg};
+    border-radius: ${theme.borderRadius.round};
+    font-family: "Amatic SC", cursive;
+
+    h1 {
+      color: ${theme.colors.greyBlue};
+      font-size: ${theme.fonts.size.P5};
+    }
+
+    h2 {
+      margin: 20px 10px 10px;
+      color: ${theme.colors.greyBlue};
+      font-size: ${theme.fonts.size.P4};
+      font-weight: ${theme.fonts.weights.regular};
+      word-wrap: normal;
+    }
+    .generate-button {
+      margin-top: 31px;
+      width: 224.05px;
+      font-size: ${theme.fonts.size.XS};
+      line-height: ${theme.fonts.size.XS};
+    }
+  }
 `;
